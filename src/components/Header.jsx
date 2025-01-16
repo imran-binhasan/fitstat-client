323253d8d8b9e9752d638f36fb0792625e94a36a
imran-binhasan <imranbinhasan.work@gmail.com>
2025-01-16 19:59:35 +0600

    update db

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <NavLink to="/classes">Classes</NavLink>
       <NavLink to="/trainers">Trainers</NavLink>
       <NavLink to="/community">Community</NavLink>
-      {user?<NavLink to="/dashboard">Dashboard</NavLink>:''}
+      {user?<NavLink to="/dashboard/balance">Dashboard</NavLink>:''}
         
 
     </>
@@ -23,9 +23,9 @@ const Header = () => {
         <div>
           <h3 className="text-2xl medium text-gray-800">fitStat</h3>
         </div>
-        <ul className="flex gap-4 items-center ">{middleLinks}
+        <ul className="flex gap-4 items-center nav-middle">{middleLinks}
         </ul>
-        <div>
+        <div className="nav-left">
         <NavLink to="/login">Login</NavLink>
         </div>
       </div>
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -7,25 +7,30 @@ import { MdAddChart, MdOutlinePlayLesson, MdOutlineUnsubscribe } from "react-ico
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-  const isAdmin = 'true';
-  const isTrainer = 'false';
+  const isAdmin = true;
+  const isTrainer = false;
   return (
     <div className="h-screen  flex justify-between ">
-      <div className="w-1/6 border  space-y-4">
-      <h3 className="text-2xl  text-center ">fitStat</h3>
-      <ul className="flex flex-col  px-4 text-lg space-y-4">
+      <div className="w-1/6 border py-4 space-y-4 fixed">
+      <h3 className="text-2xl  text-center font-medium">fitStat</h3>
+      <ul className="flex flex-col dashboard-ul px-4 space-y-4 ">
         {isAdmin && 
         <>
+        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/home-admin"><IoHomeOutline className="text-xl"/> Home Admin</NavLink>
         <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/balance"><FaMoneyCheck /> Balance</NavLink>
         <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/subscribers"><MdOutlineUnsubscribe /> Subscribers</NavLink>
         <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/all-trainers"><IoPeople /> All Trainers</NavLink>
         <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/applied-trainers"><BiMessageAltDetail /> Applied Trainers</NavLink>
-        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/new-class"><MdAddChart /> Add Class</NavLink>
+        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/add-class"><MdAddChart /> Add Class</NavLink>
         </>}
         {isTrainer && 
         <>
-
+        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/home-trainer"><IoHomeOutline className="text-xl"/> Home Trainer</NavLink>
+        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/slots"><FaMoneyCheck /> Manage Slots</NavLink>
+        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/add-slot"><MdOutlineUnsubscribe />Add Slots</NavLink>
+        <NavLink className=' px-2 py-1.5 flex gap-2 items-center' to="/dashboard/add-forum"><IoPeople /> Add Forum</NavLink>
         </>}
+        <div className="border-b-2 w-[254px] mt-2 mb-3 -ml-4 "></div>
         <NavLink className='px-2 py-1.5 flex gap-2 items-center' to="/"><IoHomeOutline className="text-xl"/> Home</NavLink>
         <NavLink className='px-2 py-1.5 flex gap-2 items-center' to="/classes"><MdOutlinePlayLesson className="text-xl"/>Classes</NavLink>
         <NavLink className='px-2 py-1.5 flex gap-2 items-center ' to="/trainers"><GrUserManager className="text-xl"/>Trainers</NavLink>
@@ -33,7 +38,7 @@ const Dashboard = () => {
       </ul>
      
       </div>
-      <div className="flex-1 border">
+      <div className="flex-1 border w-5/6  ml-[17%] ">
         <Outlet/>
       </div>
     </div>
