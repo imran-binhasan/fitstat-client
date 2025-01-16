404f89808f3b72e4ae2d697e983bf0729c822d0b
imran-binhasan <imranbinhasan.work@gmail.com>
2025-01-16 09:48:44 +0600

    update routes

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            Hp
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import Classes from "../pages/Classes/Classes";
+import Trainers from "../pages/Trainers/Trainers";
+import Community from "../pages/Community/Community";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main/>,
+        children:[
+            {
+                path:'',
+                element:<Home/>
+            },
+            {
+                path:'/classes',
+                element:<Classes/>
+            },
+            {
+                path:'/trainers',
+                element:<Trainers/>
+            },
+            {
+                path:'/community',
+                element:<Community/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/register',
+                element:<Register/>
+            },
+        ]
+    }
+])
+export default Router;
